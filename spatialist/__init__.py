cc622f06e3853cb3e73e44ed41934cad254c61fc
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-08-06 16:01:47 +0200

    [init] added check of GDAL import and version

--- spatialist/__init__.py
+++ spatialist/__init__.py
@@ -1,4 +1,14 @@
 
+try:
+    from osgeo import gdal
+    if gdal.__version__ < '2.1':
+        raise ImportError('GDAL version < 2.1. Please refer to the installation isntructions under\n'
+                          '    https://github.com/johntruckenbrodt/spatialist')
+except ImportError:
+    raise ImportError('could not import GDAL. You can install it like this:\n'
+                      '    Linux   : sudo apt-get install python3-gdal\n'
+                      '    Anaconda: conda install gdal')
+
 from . import vector
 from . import envi
 from . import ancillary
