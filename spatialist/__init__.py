67269b3626682e18b88cde2351d6525ad28a0ab4
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-08-06 13:24:48 +0200

    [init] directly import submodules

--- spatialist/__init__.py
+++ spatialist/__init__.py
@@ -1,7 +1,12 @@
 
+from . import vector
+from . import envi
+from . import ancillary
+from . import raster
+from . import sqlite_util
 from .auxil import crsConvert, haversine, gdalbuildvrt, gdalwarp, gdal_translate, ogr2ogr, gdal_rasterize
+
 from .vector import Vector, bbox, centerdist, intersect
 from .raster import Raster, stack, rasterize, dtypes
 from .sqlite_util import sqlite_setup, sqlite3
-from . import envi
-from . import ancillary
+
