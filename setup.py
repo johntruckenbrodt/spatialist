671463b96d180338b920bac2e3aeede253feaa10
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-08-06 15:50:36 +0200

    [setup] removed creating custom directory .spatialist in Home directory

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages
 import os
 
-# Create .spatialist in HOME - Directory
-directory = os.path.join(os.path.expanduser('~'), '.spatialist')
-
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
