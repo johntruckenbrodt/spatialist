b72d77cb404e4e8e640a4d5c81120240ac5b0700
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-06-15 21:52:05 +0200

    version 0.2.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 setup(name='spatialist',
       packages=find_packages(),
       include_package_data=True,
-      version='0.2.8',
+      version='0.2.9',
       description='A Python module for spatial data handling',
       classifiers=[
           'Programming Language :: Python',
