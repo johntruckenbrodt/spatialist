264045ccc9f9068959ceab89df35ba7ca2eb2084
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-08-06 10:00:43 +0200

    [__init__] added __version__ variable

--- spatialist/__init__.py
+++ spatialist/__init__.py
@@ -8,3 +8,5 @@ from .auxil import crsConvert, haversine, gdalbuildvrt, gdalwarp, gdal_translate
 from .vector import Vector, bbox, centerdist, intersect
 from .raster import Raster, stack, rasterize
 from .sqlite_util import sqlite_setup, sqlite3
+
+__version__ = '0.2.9'
