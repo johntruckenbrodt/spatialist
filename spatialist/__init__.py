c6f965795b5846d2152814a08e63e348409bf640
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-09-27 14:53:30 +0200

    [__init__] read version via pkg_resources

--- spatialist/__init__.py
+++ spatialist/__init__.py
@@ -9,4 +9,9 @@ from .vector import Vector, bbox, centerdist, intersect
 from .raster import Raster, stack, rasterize
 from .sqlite_util import sqlite_setup, sqlite3
 
-__version__ = '0.2.9'
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
